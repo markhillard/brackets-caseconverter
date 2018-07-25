2d8d0bdc506d61b349995210ec01837d6ef5c9fd
markhillard <markhillard@gmail.com>
2018-07-24 21:25:48 -0400

    Add support for multiple selection conversion - close #1

--- main.js
+++ main.js
@@ -14,13 +14,15 @@ define(function (require, exports, module) {
     function convertCase(toCase) {
         var editor = EditorManager.getFocusedEditor();
         if (editor) {
-            var selectedText = editor.getSelectedText();
-            if (toCase === 'l') {
-                selectedText = selectedText.toLowerCase();
-            } else {
-                selectedText = selectedText.toUpperCase();
-            }
-            editor._codeMirror.replaceSelection(selectedText);
+            var selectedText = editor._codeMirror.getSelections(),
+                newText = selectedText.map(function (text) {
+                    if (toCase === 'l') {
+                        return text.toLowerCase();
+                    } else {
+                        return text.toUpperCase();
+                    }
+                });
+            editor._codeMirror.replaceSelections(newText);
         }
     }
     
@@ -28,8 +30,8 @@ define(function (require, exports, module) {
     var TO_LOWER_CASE = 'markhillard.brackets.caseconverter.toLowerCase',
         TO_UPPER_CASE = 'markhillard.brackets.caseconverter.toUpperCase';
         
-    CommandManager.register('To Lower Case', TO_LOWER_CASE, function () { convertCase('l') });
-    CommandManager.register('To Upper Case', TO_UPPER_CASE, function () { convertCase('u') });
+    CommandManager.register('To Lower Case', TO_LOWER_CASE, function () { convertCase('l'); });
+    CommandManager.register('To Upper Case', TO_UPPER_CASE, function () { convertCase('u'); });
     
     // add key bindings
     KeyBindingManager.addBinding(TO_LOWER_CASE, 'Ctrl-Alt-Shift-L');
